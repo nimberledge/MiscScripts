27ddbeea653ffd789db1c2d4223c1bc181adc156
akhil <akhil2@illinois.edu>
2019-01-24 18:26:25 -0600

    shell script commit

--- one-line-camelcaser.py
+++ one-line-camelcaser.py
@@ -1,7 +1,7 @@
 from string import *
 def camel_case(s):
-    return lower(s[0:s.index(" ")]) + "".join([capitalize(i) for i in (s[s.index(" ")+1:].split())])
+    return lower(s[0:s.index(" ")]) + "".join([capitalize(i) for i in (s[s.index(" ")+1:].split())]) if (' ' in s) else s.lower()
 
 if __name__ == "__main__":
-    inp = "tHIS IS a TEST of CAMEL case"
+    inp = "tHIS is A REAL TEst"
     print inp, "\n", camel_case(inp)
